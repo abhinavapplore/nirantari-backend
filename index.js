57ebcd12121179a492915b5081d86a2a0e3cdd00
somethingofscientist <1shraddhanand@gmail.com>
2023-11-03 23:50:36 +0530

    cors added -f

--- index.js
+++ index.js
@@ -8,25 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
-
-
-
 app.use(require("./route"));
-
-app.post('/', (req, res) => {
-  return res.status(200).send({
-    success:true,
-    message: "hi backend server"
-  })
-})
-app.post('/testing', (req, res) => {
-    return res.status(200).send({
-      success:true,
-      message: "hi testing server"
-    })
-  })
-
-
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at ${process.env.PORT}`);

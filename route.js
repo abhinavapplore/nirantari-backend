c60ab965001ee408c955bb654338bbf767639873
somethingofscientist <1shraddhanand@gmail.com>
2023-11-04 00:12:25 +0530

    cors added again

--- route.js
+++ route.js
@@ -5,4 +5,12 @@ const router = express.Router();
 
 router.post("/subscriber",subscribe.subscriber);
 
+router.get("/healthCheck",(req, res) => {
+
+    console.log('hi ama health');
+    return res.status(200).json({
+        message:"health ok serivce urn"
+    })
+});
+
 module.exports = router;

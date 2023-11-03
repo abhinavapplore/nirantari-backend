dc4e1cdfb17227fde0a74138d6dd55f6bbb23d72
somethingofscientist <1shraddhanand@gmail.com>
2023-11-03 23:30:26 +0530

    nkcn

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const { connection } = require('./db/Connection');
 const router = require('./route')
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 
 

e263a931d77db3ec4bb301d4a4860edadb1968dd
somethingofscientist <1shraddhanand@gmail.com>
2023-11-03 23:59:10 +0530

    s

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+
+
 app.use(cors(
-  origin= 'http://localhost:9000',
+  origin= 'http://localhost:3000',
   methods= ['GET', 'POST'],
   allowedHeaders= ['Content-Type', 'Authorization'],
   ))
+
 app.use(express.urlencoded({ extended: true }));
 app.use(require("./route"));
 
